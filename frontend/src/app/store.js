e172c09a7d9c9bc738c014f9fbe66a6962146d4e
priyanshu-Raj26 <er.priyansu@gmail.com>
2026-07-27 01:47:50 +0530

    add Milestone API layer and Redux slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import projectReducer from "../features/projects/projectSlice";
+import milestoneReducer from "../features/milestones/milestoneSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     projects: projectReducer,
+    milestones: milestoneReducer,
   },
 });
 
