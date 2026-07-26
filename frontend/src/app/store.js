9c0dfe79ac3f5f865e8ed33400119abe93c2513d
priyanshu-Raj26 <er.priyansu@gmail.com>
2026-07-26 23:14:31 +0530

    add project API layer and Redux slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import projectReducer from "../features/projects/projectSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    projects: projectReducer,
   },
 });
 
