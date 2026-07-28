801d4ca2d7a7dd3ae66af11b1db68a2c564da1d6
priyanshu-Raj26 <er.priyansu@gmail.com>
2026-07-28 20:08:13 +0530

    add Site Progress API layer and Redux slice

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import projectReducer from "../features/projects/projectSlice";
 import milestoneReducer from "../features/milestones/milestoneSlice";
+import siteProgressReducer from "../features/siteProgress/siteProgressSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     projects: projectReducer,
     milestones: milestoneReducer,
+    siteProgress: siteProgressReducer,
   },
 });
 
